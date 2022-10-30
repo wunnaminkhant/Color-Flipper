8b196f011d0100d360753a3f6cb1558d33688df2
jackrocco <wunnaminkhant01@gmail.com>
2022-10-30 08:46:26 +0630

    hex.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", () => {
     // get random number between 0 - 3
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
